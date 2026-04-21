a4aba62be58d40a4231487628579062f0547cee8
Jonatan <jonsorf2@gmail.com>
2026-04-20 20:19:09 -0600

    Crear directorios de runtime para render

--- docker/start.sh
+++ docker/start.sh
@@ -1,6 +1,12 @@
 #!/usr/bin/env sh
 set -eu
 
+mkdir -p storage/framework/views
+mkdir -p storage/framework/cache
+mkdir -p storage/framework/sessions
+mkdir -p storage/logs
+mkdir -p bootstrap/cache
+
 php artisan migrate --force
 php artisan config:cache
 
